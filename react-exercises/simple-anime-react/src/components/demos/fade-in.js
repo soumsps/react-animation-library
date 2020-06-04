02733dee110b28818888cd3531cf4b0a71861011
Aswin <aswin.harikumar@yahoo.com>
2020-06-05 00:52:32 +0530

    Add fadein effect

--- react-exercises/simple-anime-react/src/components/demos/fade-in.js
+++ react-exercises/simple-anime-react/src/components/demos/fade-in.js
@@ -1,8 +1,8 @@
 import React from 'react';
-//import { FadeIn } from '../simple-anime-lib';
+import { FadeIn } from '../simple-anime-lib';
 import DemoPic from './demo-pic.jpg';
 const FadeInDemo = () => {
-  return <div className="demo-content">FadeIn demo page</div>;
+  return <div className="demo-content">FadeIn <FadeIn duration="2000"> demo  </FadeIn>  page</div>;
 };
 
 export default FadeInDemo;
--- react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-in.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-in.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const FadeIn = () => {
-  return <div></div>;
+const FadeIn = ({ children, duration = 5000}) => {
+  const [opacity, setOpacity] = React.useState(0.0);
+// to differentiate between first and the rest.
+  let startTime = -1.0;
+  const requestRef = React.useRef();
+  const previousTimeRef = React.useRef();
+  const animate = (time) => {
+    if (previousTimeRef.current != undefined) {
+      if (startTime < 0) {
+        startTime = new Date().getTime();
+        setOpacity(0.0 / duration);
+      } else if (time <= duration) {
+        setOpacity(time / duration); 
+      }
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+  React.useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []); // to make sure request animation only runs once at first
+  return <span style={{ opacity: opacity }}>{children}</span>;
 };
-
 export { FadeIn };
