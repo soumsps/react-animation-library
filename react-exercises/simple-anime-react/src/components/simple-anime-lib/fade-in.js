5beb1cd067478d2569daff6049942da896cfbb03
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-06-05 18:28:33 +0530

    create custom hook for doing animation

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-in.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-in.js
@@ -8,47 +8,47 @@ const FadeIn = memo((props) => {
     ? props.isInfiniteAnimation
     : false;
   const elementRef = useRef({ style: { opacity: 0.0 } });
-  const rafRef = useRef();
 
-  const timing = useCallback((timeFraction) => {
+  const timing = (timeFraction) => {
     return timeFraction;
-  }, []);
+  };
 
-  const draw = useCallback((progress) => {
+  const draw = (progress) => {
     console.log('drawing');
     if (elementRef.current !== null) {
       elementRef.current.style.opacity = progress;
     }
-  }, []);
+  };
 
-  const animate = useCallback(({ duration, timing, draw, isInfiniteAnimation }) => {
-    var start = performance.now();
-
-    rafRef.current = requestAnimationFrame(function animate(time) {
-      var timeFraction = (time - start) / duration;
-      if (timeFraction > 1) {
-        if (isInfiniteAnimation) {
-          start = time;
-          timeFraction = 0.0;
-        } else {
-          timeFraction = 1;
+  const useAnimationFrame = ({ delay, duration, timing, draw, isInfiniteAnimation }) => {
+    const rafRef = useRef();
+    const animate = useCallback(() => {
+      var start = performance.now();
+      rafRef.current = requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / duration;
+        if (timeFraction > 1) {
+          if (isInfiniteAnimation) {
+            start = time;
+            timeFraction = 0.0;
+          } else {
+            timeFraction = 1;
+          }
         }
-      }
+        var progress = timing(timeFraction);
+        draw(progress);
+        if (timeFraction < 1) {
+          rafRef.current = requestAnimationFrame(animate);
+        }
+      });
+    }, [duration, timing, draw, isInfiniteAnimation]);
 
-      var progress = timing(timeFraction);
-      draw(progress);
+    useEffect(() => {
+      setTimeout(() => animate({ duration, timing, draw, isInfiniteAnimation }), delay);
+      return () => cancelAnimationFrame(rafRef.current);
+    }, [animate, delay, duration, timing, draw, isInfiniteAnimation]);
+  };
 
-      if (timeFraction < 1) {
-        rafRef.current = requestAnimationFrame(animate);
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => animate({ duration, timing, draw, isInfiniteAnimation }), delay);
-    return () => cancelAnimationFrame(rafRef.current);
-  }, [animate, delay, duration, timing, draw, isInfiniteAnimation]);
-
+  useAnimationFrame({ delay, duration, timing, draw, isInfiniteAnimation });
   return (
     <span ref={elementRef} style={elementRef.current.style}>
       {props.children}
