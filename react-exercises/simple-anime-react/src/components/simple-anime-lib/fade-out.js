ab645d2e7bc208d7d45130ce299f08653ed256f5
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-06-05 20:15:40 +0530

    refactor fadeOut effect to use requestAnimationFrame based implementation

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-out.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/fade-out.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import './fade-out.style.css';
+import React, { useRef, memo } from 'react';
 import { DEFAULT_DELAY, DEFAULT_DURATION } from './constants';
+import { useAnimationFrame } from './helper';
 
-const FadeOut = (props) => {
+const FadeOut = memo((props) => {
   const delay = props.delay ? Number(props.delay) : DEFAULT_DELAY;
   const duration = props.duration ? Number(props.duration) : DEFAULT_DURATION;
+  const isInfiniteAnimation = props.isInfiniteAnimation
+    ? props.isInfiniteAnimation
+    : false;
+  const elementRef = useRef({ style: { opacity: 1.0 } });
+
+  const timing = (timeFraction) => {
+    return timeFraction;
+  };
+
+  const draw = (progress) => {
+    console.log('drawing');
+    if (elementRef.current !== null) {
+      elementRef.current.style.opacity = 1 - progress;
+    }
+  };
+
+  useAnimationFrame({ delay, duration, timing, draw, isInfiniteAnimation });
 
   return (
-    <span
-      style={{
-        animation: `fadeOut ease ${duration}ms ${delay}ms Infinite`,
-      }}
-    >
+    <span ref={elementRef} style={elementRef.current.style}>
       {props.children}
     </span>
   );
-};
+});
 
 export { FadeOut };
