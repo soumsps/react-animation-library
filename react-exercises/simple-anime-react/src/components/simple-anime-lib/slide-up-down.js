312d1c5a8c6c96a217766b4cb531b13ddba7a440
Aswin <aswin.harikumar@yahoo.com>
2020-06-06 16:38:43 +0530

    finish slide up down

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/slide-up-down.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/slide-up-down.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useRef, memo } from 'react';
+import { useAnimationFrame, timingFunction } from './helper';
+import { DEFAULT_SLIDEUP_CONFIG } from './constants';
 
-const SlideUpDown = () => {
-  return <div></div>;
-};
+const SlideUpDown = memo((props) => {
+  const delay = props.delay
+    ? Number(props.delay)
+    : DEFAULT_SLIDEUP_CONFIG.delay;
+  const duration = props.duration
+    ? Number(props.duration)
+    : DEFAULT_SLIDEUP_CONFIG.duration;
+  const isInfiniteAnimation = props.isInfiniteAnimation
+    ? props.isInfiniteAnimation
+    : DEFAULT_SLIDEUP_CONFIG.isInfiniteAnimation;
+  const easingStyle = props.easingStyle
+    ? props.easingStyle
+    : DEFAULT_SLIDEUP_CONFIG.easingStyle;
+  const dropHeight = props.dropHeight ? Number(props.dropHeight) : -20;
 
+  const elementRef = useRef({
+    style: { position: 'relative', top: `0px` },
+  });
+
+  let timing = timingFunction[DEFAULT_SLIDEUP_CONFIG.easingStyle];
+
+  const draw = (progress) => {
+    if (elementRef.current !== null) {
+      if (progress < 0.5) {
+        const up = dropHeight * (1 / 0.5) * progress;
+        elementRef.current.style.top = up + 'px';
+      } else {
+        const d = dropHeight * ((1 / 0.5) * (1 - progress));
+        elementRef.current.style.top = d + 'px';
+      }
+    }
+  };
+
+  if (timingFunction[easingStyle]) {
+    timing = timingFunction[easingStyle];
+  }
+  useAnimationFrame({ delay, duration, timing, draw, isInfiniteAnimation });
+  return (
+    <span ref={elementRef} style={elementRef.current.style}>
+      {props.children}
+    </span>
+  );
+});
 export { SlideUpDown };
