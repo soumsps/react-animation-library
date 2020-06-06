69266cf15d19b5b60cce2451bc1c524afe2615b5
Aswin <aswin.harikumar@yahoo.com>
2020-06-06 13:28:30 +0530

    modify constants for readability

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
@@ -1,21 +1,16 @@
 import React, { useRef, memo } from 'react';
 import { useAnimationFrame, timingFunction } from './helper';
-import {
-  DEFAULT_DELAY,
-  DEFAULT_DURATION,
-  DEFAULT_IS_INFINITE_ANIMATION,
-  DEFAULT_EASE_STYLE
-} from "./constants";
+import { DEFAULT_PULSE_CONFIG } from "./constants";
 const Pulse  = memo((props) => {
-  const delay = props.delay ? Number(props.delay) : DEFAULT_DELAY;
-  const duration = props.duration ? Number(props.duration) : DEFAULT_DURATION;
+  const delay = props.delay ? Number(props.delay) : DEFAULT_PULSE_CONFIG.delay;
+  const duration = props.duration ? Number(props.duration) : DEFAULT_PULSE_CONFIG.duration;
   const isInfiniteAnimation = props.isInfiniteAnimation
     ? props.isInfiniteAnimation
-    : DEFAULT_IS_INFINITE_ANIMATION;
-  const easingStyle = props.easingStyle ? props.easingStyle : DEFAULT_EASE_STYLE;
+    : DEFAULT_PULSE_CONFIG.easingStyle;
+  const easingStyle = props.easingStyle ? props.easingStyle : DEFAULT_PULSE_CONFIG.easingStyle;
 
   const elementRef = useRef({style: {transform: 'scale3d(1,1,1)'}});
-  let timing = timingFunction[DEFAULT_EASE_STYLE];
+  let timing = timingFunction[DEFAULT_PULSE_CONFIG.easingStyle];
   const draw = (progress) => {
     if (elementRef.current != null) {
     if (progress < 0.5) {
