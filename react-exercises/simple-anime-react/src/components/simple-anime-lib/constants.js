a0f6328ad8f0ac45b2c08631cc9861219cc554a5
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-06-06 15:00:08 +0530

    update pulse constants

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/constants.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/constants.js
@@ -1,38 +1,38 @@
-
 const DEFAULT_FADEIN_CONFIG = {
-    delay: 1000,
-    duration: 2000,
-    isInfiniteAnimation: false,
-    easingStyle: 'linear'
-  };
-  const DEFAULT_FADEOUT_CONFIG = {
-    delay: 1000,
-    duration: 2000,
-    isInfiniteAnimation: false,
-    easingStyle: 'linear'
-  };
-  const DEFAULT_BOUNCE_CONFIG = {
-    delay: 1000,
-    duration: 2000,
-    isInfiniteAnimation: false,
-    easingStyle: 'bounce'
-  };
-  const DEFAULT_PULSE_CONFIG = {
-    delay: 1000,
-    duration: 2000,
-    isInfiniteAnimation: false,
-    easingStyle: 'linear'
-  };
-  const DEFAULT_SLIDEUP_CONFIG = {
-    delay: 1000,
-    duration: 2000,
-    isInfiniteAnimation: false,
-    easingStyle: 'linear'
-  };
+  delay: 1000,
+  duration: 2000,
+  isInfiniteAnimation: false,
+  easingStyle: 'linear',
+};
+const DEFAULT_FADEOUT_CONFIG = {
+  delay: 1000,
+  duration: 2000,
+  isInfiniteAnimation: false,
+  easingStyle: 'linear',
+};
+const DEFAULT_BOUNCE_CONFIG = {
+  delay: 1000,
+  duration: 2000,
+  isInfiniteAnimation: false,
+  easingStyle: 'bounce',
+};
+const DEFAULT_PULSE_CONFIG = {
+  delay: 1000,
+  duration: 2000,
+  isInfiniteAnimation: false,
+  easingStyle: 'linear',
+  scale: 1.2,
+};
+const DEFAULT_SLIDEUP_CONFIG = {
+  delay: 1000,
+  duration: 2000,
+  isInfiniteAnimation: false,
+  easingStyle: 'linear',
+};
 export {
   DEFAULT_FADEIN_CONFIG,
   DEFAULT_BOUNCE_CONFIG,
   DEFAULT_FADEOUT_CONFIG,
   DEFAULT_PULSE_CONFIG,
-  DEFAULT_SLIDEUP_CONFIG
+  DEFAULT_SLIDEUP_CONFIG,
 };
