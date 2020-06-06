6f44d83d3c3585ff83d76f514eb739c7dc3f114d
Aswin <aswin.harikumar@yahoo.com>
2020-06-06 10:10:56 +0530

    pulse animation with  css

--- react-exercises/simple-anime-react/src/components/demos/pulse.js
+++ react-exercises/simple-anime-react/src/components/demos/pulse.js
@@ -1,8 +1,33 @@
 import React from 'react';
-//import { Pulse } from '../simple-anime-lib';
-//import DemoPic from './demo-pic.jpg';
+import { Pulse } from '../simple-anime-lib';
+import DemoPic from './demo-pic.jpg';
 const PulseDemo = () => {
-  return <div className="demo-content">Pulse demo page</div>;
+   
+  return <div className="demo-content">
+  <h1>Pulse</h1>
+  <p>The pulse animation adds a pulse effect to </p>
+      <div>
+        <h3>
+          This demo page showcases the 
+          <Pulse
+            duration={1000}
+            delay={2000}
+            isInfiniteAnimation={true}
+            >
+            pulse
+          </Pulse>
+          effect.
+        </h3>
+        <p>
+          Simple animation library that have the following things. - Bounce - FadeIn and
+          Out with easing effects - Slide up to limit and come back to original location -
+          Pulse
+        </p>
+        <Pulse duration={1000} delay={1000} isInfiniteAnimation={true}>
+          <img src={DemoPic} alt="effect demo"></img>
+        </Pulse>
+      </div>
+    </div>
 };
 
 export default PulseDemo;
--- react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
@@ -1,7 +1,26 @@
-import React from 'react';
-
-const Pulse = () => {
-  return <div></div>;
+import React from "react";
+import {
+  DEFAULT_DELAY,
+  DEFAULT_DURATION,
+  DEFAULT_IS_INFINITE_ANIMATION,
+} from "./constants";
+const Pulse = (props) => {
+  const delay = props.delay ? Number(props.delay) : DEFAULT_DELAY;
+  const duration = props.duration ? Number(props.duration) : DEFAULT_DURATION;
+  const isInfiniteAnimation = props.isInfiniteAnimation
+    ? props.isInfiniteAnimation
+    : DEFAULT_IS_INFINITE_ANIMATION;
+  return (
+    <div
+      style={{
+        animation: `pulse ${duration}ms ${delay}ms ${
+          isInfiniteAnimation ? "infinite" : "1"
+        }`,
+      }}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export { Pulse };
