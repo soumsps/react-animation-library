403e67f88f195edf5aa7834bebbc608007b99b00
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-06-06 13:08:19 +0530

    update bounce timing function

--- react-exercises/simple-anime-react/src/components/simple-anime-lib/helper.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/helper.js
@@ -37,9 +37,12 @@ const timingFunction = {
   },
 
   bounce: function bounce(timeFraction) {
+    timeFraction = 1 - timeFraction;
     for (let a = 0, b = 1; 1; a += b, b /= 2) {
       if (timeFraction >= (7 - 4 * a) / 11) {
-        return -1 * Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
+        return (
+          1 - (-1 * Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2))
+        );
       }
     }
   },
