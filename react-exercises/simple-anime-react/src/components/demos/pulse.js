1a99245747de3e8983e508980c2b9a0982b63e68
Aswin <aswin.harikumar@yahoo.com>
2020-06-06 12:54:05 +0530

    finish pulse animation

--- react-exercises/simple-anime-react/src/components/demos/pulse.js
+++ react-exercises/simple-anime-react/src/components/demos/pulse.js
@@ -3,31 +3,30 @@ import { Pulse } from '../simple-anime-lib';
 import DemoPic from './demo-pic.jpg';
 const PulseDemo = () => {
    
-  return <div className="demo-content">
+  return (
+  <div className="demo-content">
   <h1>Pulse</h1>
-  <p>The pulse animation adds a pulse effect to </p>
+  <p>This pulse effect adds the pulse animation effect to the element</p>
+  <h3>Usage</h3>
+  <h3>Examples</h3>
       <div>
         <h3>
           This demo page showcases the 
           <Pulse
-            duration={1000}
+            duration={5000}
             delay={2000}
-            isInfiniteAnimation={true}
+            isInfiniteAnimation={false}
+            easingStyle={'linear'}
             >
             pulse
           </Pulse>
           effect.
         </h3>
-        <p>
-          Simple animation library that have the following things. - Bounce - FadeIn and
-          Out with easing effects - Slide up to limit and come back to original location -
-          Pulse
-        </p>
-        <Pulse duration={1000} delay={1000} isInfiniteAnimation={true}>
+        <Pulse duration={1000} delay={1000} isInfiniteAnimation={false} easingStyle={'linear'}>
           <img src={DemoPic} alt="effect demo"></img>
         </Pulse>
       </div>
-    </div>
-};
+      </div>
+)};
 
 export default PulseDemo;
--- react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
+++ react-exercises/simple-anime-react/src/components/simple-anime-lib/pulse.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useRef, memo } from 'react';
+import { useAnimationFrame, timingFunction } from './helper';
 import {
   DEFAULT_DELAY,
   DEFAULT_DURATION,
   DEFAULT_IS_INFINITE_ANIMATION,
+  DEFAULT_EASE_STYLE
 } from "./constants";
-const Pulse = (props) => {
+const Pulse  = memo((props) => {
   const delay = props.delay ? Number(props.delay) : DEFAULT_DELAY;
   const duration = props.duration ? Number(props.duration) : DEFAULT_DURATION;
   const isInfiniteAnimation = props.isInfiniteAnimation
     ? props.isInfiniteAnimation
     : DEFAULT_IS_INFINITE_ANIMATION;
+  const easingStyle = props.easingStyle ? props.easingStyle : DEFAULT_EASE_STYLE;
+
+  const elementRef = useRef({style: {transform: 'scale3d(1,1,1)'}});
+  let timing = timingFunction[DEFAULT_EASE_STYLE];
+  const draw = (progress) => {
+    if (elementRef.current != null) {
+    if (progress < 0.5) {
+      elementRef.current.style.transform = `scale3d(${progress*1.5*2},${progress*1.5*2},${progress*1.5*2})`;
+    } else if ( progress > 0.6) {
+      elementRef.current.style.transform = `scale3d(${(1-progress)*1*2},${(1-progress)*1*2},${(1-progress)*1*2})`;
+    }
+  }
+};
+  if (timingFunction[easingStyle]) {
+    timing = timingFunction[easingStyle];
+  }
+  useAnimationFrame({ delay, duration, timing, draw, isInfiniteAnimation });
   return (
-    <div
-      style={{
-        animation: `pulse ${duration}ms ${delay}ms ${
-          isInfiniteAnimation ? "infinite" : "1"
-        }`,
-      }}
-    >
+    <div ref={elementRef} style={elementRef.current.style}  >
       {props.children}
     </div>
   );
-};
+});
 
 export { Pulse };
